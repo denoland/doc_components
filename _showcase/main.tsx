c9836fa2aafde04a759217a2c511e3743da36927
Kitson Kelly <me@kitsonkelly.com>
2022-10-17 16:44:19 +1100

    make deploy happy

---
+++ _showcase/main.tsx
@@ -0,0 +1,16 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="dom.iterable" />
+/// <reference lib="dom.asynciterable" />
+/// <reference lib="deno.ns" />
+
+/** TODO(@kitsonk) This is TEMPORARY until we merge this branch in and then we
+ * should move to `main.ts` */
+
+import { start } from "$fresh/server.ts";
+import manifest from "./fresh.gen.ts";
+
+import twindPlugin from "$fresh/plugins/twind.ts";
+import twindConfig from "./twind.config.ts";
+
+await start(manifest, { plugins: [twindPlugin(twindConfig)] });
