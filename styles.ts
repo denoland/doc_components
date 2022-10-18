11f4cd2ce2488f945337c22e2795486bbcaff8ee
crowlkats <crowlkats@toaxl.com>
2022-10-18 12:45:08 +0200

    fmt

--- styles.ts
+++ styles.ts
@@ -2,40 +2,42 @@
 
 import { apply, css, type Directive, tw } from "./deps.ts";
 
-export const comrakStyles = tw`${css({
-  // code
-  ":not(pre) > code":
-    apply`font-mono text-sm py-1 px-1.5 rounded text-black bg-gray-100 dark:(text-white bg-gray-800)`,
-  pre:
-    apply`font-mono text-sm p-2.5 rounded-lg text-black bg-gray-100 dark:(text-white bg-gray-800) overflow-x-auto`,
+export const comrakStyles = tw`${
+  css({
+    // code
+    ":not(pre) > code":
+      apply`font-mono text-sm py-1 px-1.5 rounded text-black bg-gray-100 dark:(text-white bg-gray-800)`,
+    pre:
+      apply`font-mono text-sm p-2.5 rounded-lg text-black bg-gray-100 dark:(text-white bg-gray-800) overflow-x-auto`,
 
-  // general
-  a: apply`underline`,
-  h1: apply`text-xl md:text-2xl lg:text-3xl`,
-  h2: apply`text-lg md:text-xl lg:text-2xl`,
-  h3: apply`font-bold md:(text-lg font-normal) lg:(text-xl font-normal)`,
-  h4: apply`font-semibold md:(font-bold) lg:(text-lg font-normal)`,
-  h5: apply`font-italic md:(font-semibold) lg:(font-bold)`,
-  h6: apply`md:(font-italic) lg:(font-semibold)`,
-  hr: apply`m-2 border-gray(500 dark:400)`,
-  ol: apply`list-decimal lg:list-inside`,
-  p: apply`my-1`,
-  table: apply`table-auto`,
-  td: apply`p-2 border border(solid gray(500 dark:400))`,
-  th: apply`font-bold text-center`,
-  ul: apply`lg:(list-disc list-inside)`,
+    // general
+    a: apply`underline`,
+    h1: apply`text-xl md:text-2xl lg:text-3xl`,
+    h2: apply`text-lg md:text-xl lg:text-2xl`,
+    h3: apply`font-bold md:(text-lg font-normal) lg:(text-xl font-normal)`,
+    h4: apply`font-semibold md:(font-bold) lg:(text-lg font-normal)`,
+    h5: apply`font-italic md:(font-semibold) lg:(font-bold)`,
+    h6: apply`md:(font-italic) lg:(font-semibold)`,
+    hr: apply`m-2 border-gray(500 dark:400)`,
+    ol: apply`list-decimal lg:list-inside`,
+    p: apply`my-1`,
+    table: apply`table-auto`,
+    td: apply`p-2 border border(solid gray(500 dark:400))`,
+    th: apply`font-bold text-center`,
+    ul: apply`lg:(list-disc list-inside)`,
 
-  // syntax highlighting
-  ".code-comment": apply`text-gray(500 dark:400)`,
-  ".code-function": apply`text-green(700 dark:300)`,
-  ".code-literal": apply`text-cyan(600 dark:400)`,
-  ".code-keyword, .code-operator, .code-variable.code-language":
-    apply`text-purple(800 dark:300)`,
-  ".code-number, .code-doctag": apply`text-indigo(600 dark:400)`,
-  ".code-regexp": apply`text-red(700 dark:300)`,
-  ".code-string": apply`text-yellow(500 dark:200)`,
-  ".code-type, .code-built_in": apply`text-cyan(600 dark:400) italic`,
-})}`;
+    // syntax highlighting
+    ".code-comment": apply`text-gray(500 dark:400)`,
+    ".code-function": apply`text-green(700 dark:300)`,
+    ".code-literal": apply`text-cyan(600 dark:400)`,
+    ".code-keyword, .code-operator, .code-variable.code-language":
+      apply`text-purple(800 dark:300)`,
+    ".code-number, .code-doctag": apply`text-indigo(600 dark:400)`,
+    ".code-regexp": apply`text-red(700 dark:300)`,
+    ".code-string": apply`text-yellow(500 dark:200)`,
+    ".code-type, .code-built_in": apply`text-cyan(600 dark:400) italic`,
+  })
+}`;
 
 const styles = {
   anchor:
