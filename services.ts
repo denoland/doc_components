b16b75382764c66044eea85bebc279b8db61231d
crowlkats <crowlkats@toaxl.com>
2023-04-07 17:10:46 +0200

    fmt

--- services.ts
+++ services.ts
@@ -33,7 +33,12 @@ export interface Configuration {
    *
    * Implementors should return a string which will be used as the `href` value
    * for a link. */
-  resolveHref?: (url: URL, symbol?: string, namespace?: string, property?: string) => string;
+  resolveHref?: (
+    url: URL,
+    symbol?: string,
+    namespace?: string,
+    property?: string,
+  ) => string;
   /** Called when doc components are trying to generate a link to a source file.
    *
    * Implementors should return a string which which will be used as the `href`
